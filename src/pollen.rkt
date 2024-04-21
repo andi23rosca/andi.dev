7f21dbf2b6b21ea7b5d715710392463747b276f0
Andi Rosca <andi23rosca@gmail.com>
2024-04-22 00:06:37 +0200

    Initial pollen setup

---
+++ src/pollen.rkt
@@ -0,0 +1,12 @@
+#lang racket/base
+(require pollen/tag)
+(provide (all-defined-out))
+(define headline (default-tag-function 'h2))
+(define items (default-tag-function 'ul))
+(define item (default-tag-function 'li 'p))
+(define (link url text) `(a ((href ,url)) ,text))
+
+
+(module setup racket/base
+  (provide (all-defined-out))
+  (define publish-directory (build-path (current-directory) "../dist")))
---
+++ src/template.html.p
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+  <meta charset="UTF-8" />
+  <title>◊select['h2 doc], by MB</title>
+</head>
+
+<body style="background: #f6f6f6">
+  <div style="background: white; margin: 3em;
+border:10px double gray; padding: 3em; font-size: 130%;">
+    This file is ◊here
+    <hr />
+    ◊(->html doc)
+  </div>
+</body>
+
+</html>
