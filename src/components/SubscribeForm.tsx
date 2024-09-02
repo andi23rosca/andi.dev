9df4e64f860725590eab219f8a953532d7a18ba9
Andi Rosca <andi23rosca@gmail.com>
2024-09-02 11:53:59 +0200

    Fix: Load form script tag dynamically

--- src/components/SubscribeForm.tsx
+++ src/components/SubscribeForm.tsx
@@ -1,11 +1,14 @@
+import { onMount } from "solid-js";
+
 export const SubscribeForm = () => {
-	return (
-		<div>
-			<script
-				async
-				data-uid="a6d9b30e24"
-				src="https://andi-dev.ck.page/a6d9b30e24/index.js"
-			/>
-		</div>
-	);
+	let ref!: HTMLDivElement;
+	onMount(() => {
+		const el = document.createElement("script");
+		el.setAttribute("data-uid", "a6d9b30e24");
+		el.setAttribute("async", "true");
+		el.setAttribute("src", "https://andi-dev.ck.page/a6d9b30e24/index.js");
+		ref.append(el);
+	});
+
+	return <div ref={ref} />;
 };
