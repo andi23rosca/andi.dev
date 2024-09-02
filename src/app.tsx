5448d17f00b24bdcff510ff9fcb9f5017597cb0c
Andi Rosca <andi23rosca@gmail.com>
2024-09-02 22:15:25 +0200

    disable dithering for now

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,13 @@
 import { Router, useBeforeLeave, useIsRouting } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
-import { createEffect, onCleanup, onMount, Suspense } from "solid-js";
+import {
+	type Accessor,
+	createEffect,
+	createSignal,
+	onCleanup,
+	onMount,
+	Suspense,
+} from "solid-js";
 import "./app.css";
 import { Layout } from "./components/Layout";
 import { MetaProvider, Title } from "@solidjs/meta";
@@ -28,6 +35,76 @@ const useRouteTransitionTiming = (
 	});
 };
 
+const useDitherAnimation = (ref: Accessor<HTMLElement | undefined>) => {
+	if (!isServer) {
+		const d1 = document.createElement("div");
+		d1.classList.add("dither", "dither-1");
+		const d2 = document.createElement("div");
+		d2.classList.add("dither", "dither-2");
+		const d3 = document.createElement("div");
+		d3.classList.add("dither", "dither-3");
+
+		let started = false;
+		useRouteTransitionTiming(
+			300,
+			() => {
+				ref()?.appendChild(d1);
+				setTimeout(() => {
+					ref()?.appendChild(d2);
+				}, 100);
+				setTimeout(() => {
+					ref()?.appendChild(d3);
+				}, 200);
+				started = true;
+			},
+			() => {
+				const rnd = () =>
+					setTimeout(() => {
+						try {
+							d1.style.backgroundPosition = `${Math.round(
+								Math.random() * 100,
+							)}px ${Math.round(Math.random() * 100)}px`;
+							d2.style.backgroundPosition = `${Math.round(
+								Math.random() * 100,
+							)}px ${Math.round(Math.random() * 100)}px`;
+							d3.style.backgroundPosition = `${Math.round(
+								Math.random() * 100,
+							)}px ${Math.round(Math.random() * 100)}px`;
+						} catch (error) {
+							console.error(error);
+						}
+						if (started) {
+							rnd();
+						}
+					}, 100);
+				rnd();
+			},
+			() => {
+				started = false;
+				try {
+					ref()?.removeChild(d3);
+				} catch (error) {
+					console.error(error);
+				}
+				setTimeout(() => {
+					try {
+						ref()?.removeChild(d2);
+					} catch (error) {
+						console.error(error);
+					}
+				}, 100);
+				setTimeout(() => {
+					try {
+						ref()?.removeChild(d1);
+					} catch (error) {
+						console.error(error);
+					}
+				}, 200);
+			},
+		);
+	}
+};
+
 export default function App() {
 	onMount(() => {
 		const listener = (e: KeyboardEvent) => {
@@ -47,65 +124,14 @@ export default function App() {
 			<Title>andi.dev</Title>
 			<Router
 				root={(props) => {
-					let ref!: HTMLDivElement;
+					const [ref, setRef] = createSignal<HTMLDivElement>();
 
-					if (!isServer) {
-						const d1 = document.createElement("div");
-						d1.classList.add("dither", "dither-1");
-						const d2 = document.createElement("div");
-						d2.classList.add("dither", "dither-2");
-						const d3 = document.createElement("div");
-						d3.classList.add("dither", "dither-3");
-
-						let started = false;
-						useRouteTransitionTiming(
-							300,
-							() => {
-								ref.appendChild(d1);
-								setTimeout(() => {
-									ref.appendChild(d2);
-								}, 100);
-								setTimeout(() => {
-									ref.appendChild(d3);
-								}, 200);
-								started = true;
-							},
-							() => {
-								const rnd = () =>
-									setTimeout(() => {
-										d1.style.backgroundPosition = `${Math.round(
-											Math.random() * 100,
-										)}px ${Math.round(Math.random() * 100)}px`;
-										d2.style.backgroundPosition = `${Math.round(
-											Math.random() * 100,
-										)}px ${Math.round(Math.random() * 100)}px`;
-										d3.style.backgroundPosition = `${Math.round(
-											Math.random() * 100,
-										)}px ${Math.round(Math.random() * 100)}px`;
-										if (started) {
-											rnd();
-										}
-									}, 100);
-								rnd();
-							},
-							() => {
-								started = false;
-								ref.removeChild(d3);
-								setTimeout(() => {
-									ref.removeChild(d2);
-								}, 100);
-								setTimeout(() => {
-									ref.removeChild(d1);
-								}, 200);
-							},
-						);
-					}
-
+					// useDitherAnimation(ref);
 					return (
 						<Layout>
 							<Suspense>{props.children}</Suspense>
 
-							<div aria-hidden ref={ref} />
+							<div aria-hidden ref={setRef} />
 						</Layout>
 					);
 				}}
